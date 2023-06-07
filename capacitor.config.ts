53bdc7bbce020d20d53941c661f894fe0b91d634
Fran <francisco.calles@edu.uah.es>
2023-06-07 22:01:32 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Trabajo Final Hibridas',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
